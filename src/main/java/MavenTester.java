53b8dec4cf445f216d38642055f403792e767c4e
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-20 15:13:12 -0600

    fiished

--- src/main/java/MavenTester.java
+++ src/main/java/MavenTester.java
@@ -1,8 +1,21 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class MavenTester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter something and: \n a. I'll print whether or not  the user entered is a number\n b. Flip the case of the string \n c. Reverse the string");
+        String userInput = scanner.nextLine();
+
+        System.out.println("a. " + StringUtils.isNumeric(userInput));
+        System.out.println("b. " + StringUtils.swapCase(userInput));
+        System.out.println("c. " + StringUtils.reverse(userInput));
+
+
+
+
 
 
     }
